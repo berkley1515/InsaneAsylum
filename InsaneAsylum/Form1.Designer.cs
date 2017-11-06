362dc14e5db18843f2baac402e1a5057d5664338
Berkley Fair <berkley1515@gmail.com>
2017-11-06 13:34:26 -0500

    controls

--- InsaneAsylum/Form1.Designer.cs
+++ InsaneAsylum/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.outputLabel1 = new System.Windows.Forms.Label();
             this.sceneTester = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // outputLabel1
@@ -53,12 +54,24 @@
             this.sceneTester.TabIndex = 1;
             this.sceneTester.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Rockwell Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(859, 681);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(479, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Press C for 1st choice, B for middle (if 3 choices), and M for last.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.sceneTester);
             this.Controls.Add(this.outputLabel1);
             this.Name = "Form1";
@@ -73,6 +86,7 @@
 
         private System.Windows.Forms.Label outputLabel1;
         private System.Windows.Forms.Label sceneTester;
+        private System.Windows.Forms.Label label1;
     }
 }
 
